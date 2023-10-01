c1f942baf69f35ffe7e5f80c071b6946e3a36364
Diana Karuku <diana.karuku@student.moringaschool.com>
2023-10-01 19:30:41 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+const drivers= ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(drivers, name){
+  const lowercaseName = name.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === lowercaseName);
+  };
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  };
+  const driversTown = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
